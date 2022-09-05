7dee576d56e2adf340f1082194fcc35838c1323b
Enock Kiprotich <ngetichenock32@gmail.com>
2022-09-06 00:04:30 +0300

    exit

--- exit_cmd.c
+++ exit_cmd.c
@@ -1,10 +1,8 @@
 #include "shell.h"
-
 /**
 * exit_cmd - handles the exit command
 * @command: tokenized command
 * @line: input read from stdin
-*
 * Return: no return
 */
 void exit_cmd(char **command, char *line)
