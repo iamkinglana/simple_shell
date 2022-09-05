de888977d931667f713f07e633ac1bbd70cb3108
Enock Kiprotich <ngetichenock32@gmail.com>
2022-09-06 00:05:29 +0300

    Path

--- append_path.c
+++ append_path.c
@@ -4,10 +4,10 @@
 * append_path - adds path to command
 * @path: path of command
 * @command: user entered command
-*
 * Return: buffer containing command with path on success
 * NULL on failure
 */
+
 char *append_path(char *path, char *command)
 {
 	char *buf;
